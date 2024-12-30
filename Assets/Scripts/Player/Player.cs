e1effae6779169422a031bdd483485518808a152
Ksardess <adam.andrasz123@gmail.com>
2024-12-30 14:50:00 +0100

    Basic UI with testing taking Damage on Space

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHelth = 100;
+    public int currentHelth;
+
+    public HelthBar helthBar;
+
+    void Start()
+    {
+        if (helthBar == null)
+        {
+            helthBar = GetComponentInChildren<HelthBar>();
+        }
+
+        currentHelth = maxHelth;
+        helthBar.SetMaxHelth(maxHelth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHelth -= damage;
+        Debug.Log("Otrzymałeś obrażenia: " + damage);
+
+        helthBar.SetHelth(currentHelth);
+    }
+}
